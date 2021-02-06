66571e364dc5f810dca75dd6345018bc787d61b5
Praveen Sinnur <praveen@zeliot.in>
2021-02-06 15:02:33 +0530

    empty post validation done for create post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -30,6 +30,10 @@ module.exports = {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
 
+      if (body.trim() === "") {
+        throw new Error("Post body cannot be empty");
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
