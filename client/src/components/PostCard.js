c55fba94f8c385347b460399b78e748b5e767919
Praveen Sinnur <praveen@zeliot.in>
2021-02-06 17:36:58 +0530

    recent posts ui done with get all posts api integration

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Card, Icon, Label, Image, Button } from "semantic-ui-react";
+import moment from "moment";
+import { Link } from "react-router-dom";
+
+function PostCard({
+  post: { body, createdAt, id, username, likeCount, commentCount, likes },
+}) {
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button as="div" labelPosition="right">
+          <Button color="teal" basic>
+            <Icon name="heart" />
+          </Button>
+          <Label basic color="teal" pointing="left">
+            {likeCount}
+          </Label>
+        </Button>
+      </Card.Content>
+    </Card>
+  );
+}
+
+export default PostCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Grid } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
+
+const FETCH_ALL_POSTS = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+
+      likes {
+        username
+      }
+
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 function Home() {
+  const { loading, data } = useQuery(FETCH_ALL_POSTS, {
+    fetchPolicy: "network-only",
+  });
+
+  let posts;
+  if (data) {
+    posts = data.getPosts;
+  }
+
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row>
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loadin posts...</h1>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   );
 }
 
